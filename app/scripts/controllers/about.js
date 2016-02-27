8744d7363021d4cdb62c3ef1bca460f3924bc329
Diego Diaz <diego@MacBook-Pro-de-Diego.local>
2016-02-27 11:46:23 +0100

    Importación inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name presupappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the presupappApp
+ */
+angular.module('presupappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name presupappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the presupappApp
+ */
+angular.module('presupappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
